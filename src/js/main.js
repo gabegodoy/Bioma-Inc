af3592718e9592a7dfe1ed61c4e2ffede8f6dfde
godoy-nexum <gabriel.godoy@nexum.com.br>
2024-01-28 17:20:53 -0300

    project page

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,17 @@ const sidebarToggle = () => {
 // Event Listeners
 trigger.addEventListener('click', sidebarToggle);
 
+
+// LOAD MORE CARDS ON PROJECT PAGE GRID
+function showMoreCards () {
+ let projectGridViewMoreBtn = document.querySelector('#projectGridViewMore');
+ let projectGridPhotos = document.querySelectorAll('.project__page__grid__img__container');
+
+ projectGridPhotos.forEach(pic => {
+   if (pic.classList.contains('d-none')) {
+     pic.classList.remove('d-none')
+     projectGridViewMoreBtn.classList.add('d-none')
+   }
+   
+ });
+}
