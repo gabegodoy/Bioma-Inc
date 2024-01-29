ba0982698a645f41593e489cdfc788d0417b71a8
Pedro Oliveira <pedro.oliveira.cc01@gmail.com>
2024-01-29 02:52:03 -0300

    WIP home

---
+++ src/js/home.js
@@ -0,0 +1,33 @@
+var mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 8000, // Delay between slides in milliseconds (8 seconds)
+    },
+    speed: 1500, // Transition speed in milliseconds (1.5 seconds)
+    on: {
+      slideChangeTransitionStart: function () {
+        var activeSlide = this.slides[this.activeIndex];
+        setTimeout(() => {
+            activeSlide.style.transform = 'scale(1.5)';
+        }, 1500);
+      },
+      slideChangeTransitionEnd: function () {
+        var previousSlide = this.slides[this.previousIndex];
+        previousSlide.style.transform = 'scale(1)';
+      },
+    },
+    // Pagination, navigation, and scrollbar options remain the same
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar', // Usa uma barra de progresso como a página de paginação
+      
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
